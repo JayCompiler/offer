ddfeafed6d1008820c8e60e9cd84604ed8af6ed7
ZY <1273044376@qq.com>
2018-04-25 10:40:40 +0800

    third

--- offer/src/offer/_24FindPath.java
+++ offer/src/offer/_24FindPath.java
@@ -1,37 +1,61 @@
 package offer;
 
 import java.util.ArrayList;
+import java.util.Stack;
 
 public class _24FindPath {
 
 	public static void main(String[] args) {
-
-	}
-
-	public ArrayList<ArrayList<Integer>> FindPath(TreeNode root, int target) {
-		ArrayList<ArrayList<Integer>> arrayList = new ArrayList<>();
-		if (root==null)
-			return null;
-		if (root.val == target) {
-			arrayList.add(new ArrayList<>(root.val));
-			return arrayList;
-		} else {
-			remain = target - root.val;
-			if (root.left != null) {
-				helpFind(root.left, remain, arrayList);
-				
-			}
-			if (root.right != null) {
-				 helpFind(root.right, remain, arrayList);
-				
-			}
+		TreeNode a=new TreeNode(1);
+		ArrayList<ArrayList<Integer>>arrayList= FindPath(a, 1);
+		for (ArrayList<Integer> arrayList2 : arrayList) {
+			System.out.println(arrayList2);
 		}
-		return null;
 	}
 
-	private ArrayList<ArrayList<Integer>> helpFind(TreeNode left, int remain, ArrayList<ArrayList<Integer>> arrayList) {
-		return null;
+	public static ArrayList<ArrayList<Integer>> FindPath(TreeNode root, int target) {
+		ArrayList<ArrayList<Integer>> res=new ArrayList<>();
+		if(root==null) {
+			return res;
+		}
+		if(root.right==null&&root.left==null&&target==root.val) {
+			ArrayList<Integer> temp=new ArrayList<>();
+			temp.add(root.val);
+			res.add(temp);
+		    return res;
+		}
+		Stack<TreeNode> stack=new Stack<>();
+		stack.push(root);
+		if(root.left!=null)
+			helpFind(root.left, target-root.val, stack,res);
+		if(root.right!=null)
+			helpFind(root.right, target-root.val, stack, res);
+		return res;
 	}
+
+	private static void helpFind(TreeNode node, int target, Stack<TreeNode> stack, ArrayList<ArrayList<Integer>> res) {
+		stack.push(node);
+		if(node.left==null&&node.right==null&&target-node.val==0) {
+			ArrayList<Integer> arrayList=new ArrayList<>();
+			Stack<TreeNode> temp=new Stack<>();
+			while(!stack.isEmpty())
+				temp.push(stack.pop());
+			while(!temp.isEmpty()) {
+				arrayList.add(temp.peek().val);
+				stack.push(temp.pop());
+			}
+			res.add(arrayList);
+			stack.pop();
+			return;
+		}
+		if(node.left!=null)
+			helpFind(node.left, target-node.val, stack, res);
+		if(node.right!=null)
+			helpFind(node.right, target-node.val, stack, res);
+		stack.pop();
+	}
+
+
 
 
 }
---
+++ offer/src/offer/_26Convert.java
@@ -0,0 +1,20 @@
+package offer;
+
+import java.util.ArrayList;
+import java.util.Stack;
+
+public class _26Convert {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+	public TreeNode Convert(TreeNode pRootOfTree) {
+		Stack<TreeNode> stack=new Stack<>();
+		ArrayList<TreeNode> rearr=new ArrayList<>();
+		while(pRootOfTree!=null) {
+			stack.push(pRootOfTree);
+		}
+	}
+}
